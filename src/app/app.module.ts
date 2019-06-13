b6ce8945ebc67007fa43d670ff225285aaae1e48
Njabulo96 <njabulomlang96@gmail.com>
2019-06-13 11:41:07 +0200

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrangeComponent } from './pages/orange/orange.component';
+import { WhiteComponent } from './pages/white/white.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrangeComponent,
+    WhiteComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
